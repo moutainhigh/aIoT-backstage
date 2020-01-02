d490fb06f05189acf27ed67322cb768fc9ec7618
xiaowenhui <xiaowenhui@scaszh.com>
2020-01-02 11:14:45 +0800

    初始化项目

---
+++ src/main/java/com/aiot/aiotbackstage/AiotBackstageApplication.java
@@ -0,0 +1,13 @@
+package com.aiot.aiotbackstage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiotBackstageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiotBackstageApplication.class, args);
+	}
+
+}
